dcbe87e548881cb6b7029e5c921a6d7ca2c4dabe
frankbjj23 <frankbjj23@gmail.com>
2025-12-10 12:52:58 -0500

    second session

--- src/GameScreen.jsx
+++ src/GameScreen.jsx
@@ -1,35 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const GameScreen = () => {
-  const [moles, setMoles] = useState(0);
   const [score, setScore] = useState(0);
-  const moleHoles = new Array(9).fill(null);
+  const moleHoles = Array.from({ length: 9 }, (index) => index);
+  const [clickedMole, setClickedMole] = useState(() => {
+    Math.floor(Math.random() * moleHoles.length);
+  });
+  const [timeRemaining, setTimeRemaining] = useState(30);
 
-  const moleClick = () => {
+  const pickRandomMole = () => {
     const randomIndex = Math.floor(Math.random() * moleHoles.length);
-    setScore(randomIndex);
+    setClickedMole(randomIndex);
   };
+
   const handleClick = (mole) => {
-    if (mole !== moles) return;
+    if (mole !== clickedMole) return;
+    if (timeRemaining <= 0) return;
     setScore((prev) => prev + 1);
-    moleClick();
+    pickRandomMole();
   };
+
+  const handleRestart = () => {
+    setScore(0);
+    setTimeRemaining(30);
+    pickRandomMole();
+  };
+  useEffect(() => {
+    if (timeRemaining <= 0) return;
+    const time = setInterval(() => {
+      setTimeRemaining((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(time);
+  }, [timeRemaining]);
+
   return (
     <>
-      <div>
+      <div className="game-wrapper">
         <h1>Whack a Mole</h1>
-        <button>Score:</button>
-        <button>Time:</button>
-        <button>Restart</button>
-      </div>
-      <div>
-        {moleHoles.map((index) => (
-          <div
-            className={`toggleButton ${mole ? "hole" : "mole"}`}
-            onClick={handleClick}
-          />
-        ))}
-        <div />
+        <div className="game-info">
+          <button type="button">Score: {score}</button>
+          <button type="button">Time: {timeRemaining}</button>
+          <button type="button" onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+        <div className="game-board">
+          {moleHoles.map((index) => (
+            <div
+              key={index}
+              className={`toggleButton ${
+                index === clickedMole ? "mole" : "hole"
+              }`}
+              onClick={() => handleClick(index)}
+            />
+          ))}
+          <div />
+        </div>
       </div>
     </>
   );
